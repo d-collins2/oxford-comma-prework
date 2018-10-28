f05e401255cadb2c74c2fccf7c2ca610e9cb46d5
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-28 19:14:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,19 @@
 def oxford_comma(array)
   len = array.length 
-  if len == 1 
-    return "{array.first}"
-  elsif len == 2 
-    return ""
+  case 
+  when len == 1 
+    return "#{array.first}"
+  when len == 2 
+    return "#{array.first} and #{array.last}"
+  else 
+    str = ""
+    array.each_with_index do |el, idx|
+      if idx == len - 1 
+        str += "and #{el}"
+      else 
+        str += "#{el}, "
+      end 
+    str
+    end 
+  end 
 end
