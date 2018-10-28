4f00c56b1e363dbb610e698c21083fa3a448689a
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-28 19:16:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,16 +5,7 @@ def oxford_comma(array)
     return "#{array.first}"
   when len == 2 
     return "#{array.first} and #{array.last}"
-  when len == 3 
+  else len == 3 
     return "#{array.first}, #{array.1} and #{array.last}"
-    str = ""
-    array.each_with_index do |el, idx|
-      if idx == len - 1 
-        str += "and #{el}"
-      else 
-        str += "#{el}, "
-      end 
-    str
-    end 
   end 
 end
