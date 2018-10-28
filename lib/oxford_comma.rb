5151607400de021306bc3c8577d8ccb8fdc3d6a0
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-28 19:16:31 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,8 @@ def oxford_comma(array)
     return "#{array.first}"
   when len == 2 
     return "#{array.first} and #{array.last}"
-  else 
+  when len == 3 
+    return "#{array.first}, #{array.1} and #{array.last}"
     str = ""
     array.each_with_index do |el, idx|
       if idx == len - 1 
